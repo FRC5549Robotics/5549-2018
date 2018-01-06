6cda50db5239a68069779e32f7847a084b10dee4
Parsia B <parsiabahrami80@gmail.com>
2018-01-06 10:39:27 -0500

    Initial commit

---
+++ robot.py
@@ -0,0 +1,43 @@
+import wpilib
+
+
+class MyRobot(wpilib.IterativeRobot):
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+
+        self.robot_drive = wpilib.RobotDrive(0, 1, 3, 2)
+        self.robot_drive.setExpiration(0.1)
+        self.stick = wpilib.XboxController(1)
+
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.auto_loop_counter = 0
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+        # Check if we've completed 100 loops (approximately 2 seconds)
+        if self.auto_loop_counter < 100:
+            self.robot_drive.drive(-0.5, 0)  # Drive forwards at half speed
+            self.auto_loop_counter += 1
+        else:
+            self.robot_drive.drive(0, 0)  # Stop robot
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        #left_stick = self.stick.getRawAxis(1)/2
+        #right_stick = self.stick.getRawAxis(5)/2
+        left_stick = self.stick.getX(XboxController.Hand.kLeft)
+        self.robot_drive.tankDrive(left_stick, right_stick)
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
---
+++ util.py
@@ -0,0 +1,92 @@
+# RobotPy Utility
+# Benjamin Ward 2018
+# Allows easy PyCharm test/deploy of FRC RobotPy applications 
+
+import sys
+import platform
+import py
+import pytest
+import wpilib
+import hal
+
+from subprocess import call
+
+
+def _test():
+    call(["python", "robot.py", "test", "--", "-x"], shell=True)
+
+
+def _deploy():
+    if len(sys.argv) > 2:
+        if "--skip-tests" in sys.argv[2]:
+            call(["python", "robot.py", "deploy", "--skip-tests"], shell=True)
+            return
+
+    call(["python", "robot.py", "deploy"], shell=True)
+
+
+def _deploy_netconsole():
+    if len(sys.argv) > 2:
+        if "--skip-tests" in sys.argv[2]:
+            call(["python", "robot.py", "deploy", "--nc", "--skip-tests"], shell=True)
+            return
+
+    call(["python", "robot.py", "deploy", "--nc"], shell=True)
+
+
+def _update_robotpy():
+    call(["python", "-m", "robotpy_installer", "download-robotpy"], shell=True)
+    call(["python", "-m", "robotpy_installer", "download-opkg", "python36-robotpy-ctre"])
+
+
+def _install_robotpy():
+    call(["python", "-m", "robotpy_installer", "install-robotpy"], shell=True)
+    call(["python", "-m", "robotpy_installer", "install-opkg", "python36-robotpy-ctre"])
+
+
+def _install_packages():
+    pass
+
+
+def _flash():
+    _install_robotpy()
+    _install_packages()
+
+
+def _pass():
+    pass
+
+
+if __name__ == "__main__":
+    action = ""
+    if len(sys.argv) > 1:
+        action = sys.argv[1]
+
+    actions = {
+        "test": _test,
+        "deploy": _deploy,
+        "deploync": _deploy_netconsole,
+        "update": _update_robotpy,
+        "flash": _flash,
+        "": _pass
+    }
+
+    # Note: py.io is not imported in py's __init__.py but does exist.
+    # See http://py.readthedocs.io/en/latest/io.html
+
+    # Markup options are: black, red, green, yellow, blue, purple, cyan, white,
+    # Black, Red, Green, Yellow, Blue, Purpole, Cyan, White, bold, light, blink, invert
+
+    tw = py.io.TerminalWriter()
+    tw.hasmarkup = True
+
+    tw.line()
+    tw.line("Platform %s \nPython %s" % (sys.platform, platform.python_version()))
+    tw.line("pytest-%s, py-%s" % (pytest.__version__, py.__version__))
+    tw.line("wpilib-%s, hal-%s" % (wpilib.__version__, hal.__version__))
+
+    tw.sep('=', "Running action: " + (action if action != "" else "none"), bold=True, green=True)
+    try:
+        actions[action]()
+    except:
+        tw.line("No entry for " + action, bold=True, Red=True)
