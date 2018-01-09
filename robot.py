c08463c392e229da01fed3b622f6646d4c466593
Parsia B <parsiabahrami80@gmail.com>
2018-01-09 16:07:14 -0500

    Rev1.1
    Fully operational basic Tank Drive. Under development.

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import wpilib
 
 
+# noinspection PyAttributeOutsideInit
 class MyRobot(wpilib.IterativeRobot):
     def robotInit(self):
         """
@@ -10,8 +11,8 @@ class MyRobot(wpilib.IterativeRobot):
 
         self.robot_drive = wpilib.RobotDrive(0, 1, 3, 2)
         self.robot_drive.setExpiration(0.1)
-        self.stick = wpilib.XboxController(1)
-
+        #self.stick = wpilib.XboxController(1)
+        self.stick = wpilib.Joystick(1)
 
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
@@ -29,10 +30,17 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
-        #left_stick = self.stick.getRawAxis(1)/2
-        #right_stick = self.stick.getRawAxis(5)/2
+<<<<<<< HEAD
+        left_stick = self.stick.getRawAxis(1)/2
+        right_stick = self.stick.getRawAxis(5)/2
+        #left_stick = self.stick.getY(wpilib.XboxController.Hand.kLeft)
+        #right_stick = self.stick.getY(wpilib.XboxController.Hand.kRight)
+=======
+        # left_stick = self.stick.getRawAxis(1)/2
+        # right_stick = self.stick.getRawAxis(5)/2
         left_stick = self.stick.getY(wpilib.XboxController.Hand.kLeft)
         right_stick = self.stick.getX(wpilib.XboxController.Hand.kRight)
+>>>>>>> 702b4981f5dc826f94b85527b42de3a5cec6b39e
         self.robot_drive.tankDrive(left_stick, right_stick)
 
     def testPeriodic(self):
