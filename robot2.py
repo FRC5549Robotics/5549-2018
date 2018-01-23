06aeb1ab4ff4d627dff456071d8e647f68226a4c
Parsia B <parsiabahrami80@gmail.com>
2018-01-23 15:25:49 -0500

    Rev 1.2
    Fixed drive code for 2018 wpilib. Contains modifications to adjust drive speed.

--- robot2.py
+++ robot2.py
@@ -14,27 +14,36 @@ class MyRobot(wpilib.IterativeRobot):
         '''Robot initialization function'''
 
         # object that handles basic drive operations
-        self.frontLeftMotor = wpilib.Victor(0)
-        self.rearLeftMotor = wpilib.Victor(1)
-        self.frontRightMotor = wpilib.Victor(2)
-        self.rearRightMotor = wpilib.Victor(3)
+        self.frontLeftMotor = wpilib.Victor(1)
+        self.rearLeftMotor = wpilib.Victor(2)
+        self.frontRightMotor = wpilib.Victor(3)
+        self.rearRightMotor = wpilib.Victor(4)
 
         self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
         self.right = wpilib.SpeedControllerGroup(self.frontRightMotor, self.rearRightMotor)
 
-        self.myRobot = DifferentialDrive(self.left, self.right)
-        self.myRobot.setExpiration(0.1)
+        self.drive = DifferentialDrive(self.left, self.right)
+        self.drive.setExpiration(0.1)
 
         # joysticks 1 on the driver station
         self.stick = wpilib.Joystick(1)
 
     def teleopInit(self):
         '''Executed at the start of teleop mode'''
-        self.myRobot.setSafetyEnabled(True)
+        self.drive.setSafetyEnabled(True)
 
     def teleopPeriodic(self):
         '''Runs the motors with tank steering'''
-        self.myRobot.tankDrive(self.stick.getY(2) * -1, self.stick.getY(5) * -1)
+        left_stick = self.stick.getRawAxis(1)/1.5
+        right_stick = self.stick.getRawAxis(5)/1.5
+        '''if right_stick < 0:
+            divisor = 2
+        else:
+            divisor = 2.3'''
+
+        #adjusted_right_stick = right_stick/divisor
+
+        self.drive.tankDrive(-left_stick, -right_stick)
 
 if __name__ == '__main__':
     wpilib.run(MyRobot)
