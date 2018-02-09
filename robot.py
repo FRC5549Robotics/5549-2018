c28fe4845e64d89388648d3a212515c217f95665
Parsia B <parsiabahrami80@gmail.com>
2018-02-09 15:25:29 -0500

    Rev1.2.4
    Added autonomous procedures using FMS. Removed pneumatics code.

--- robot.py
+++ robot.py
@@ -1,13 +1,12 @@
 '''
     This is a demo program showing the use of the RobotDrive class,
     specifically it contains the code necessary to operate a robot with
-    tank drive.
+    tank drive and basic autonomous procedures.
 '''
 
 import wpilib
 from wpilib.drive import DifferentialDrive
-from wpilib.doublesolenoid import DoubleSolenoid
-
+from wpilib import DriverStation
 
 class MyRobot(wpilib.IterativeRobot):
 
@@ -15,10 +14,10 @@ class MyRobot(wpilib.IterativeRobot):
         '''Robot initialization function'''
 
         # object that handles basic drive operations
-        self.frontLeftMotor = wpilib.Talon(1)
-        self.rearLeftMotor = wpilib.Talon(2)
-        self.frontRightMotor = wpilib.Talon(3)
-        self.rearRightMotor = wpilib.Talon(4)
+        self.frontLeftMotor = wpilib.Victor(1)
+        self.rearLeftMotor = wpilib.Victor(2)
+        self.frontRightMotor = wpilib.Victor(3)
+        self.rearRightMotor = wpilib.Victor(4)
 
         self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
         self.right = wpilib.SpeedControllerGroup(self.frontRightMotor, self.rearRightMotor)
@@ -30,28 +29,97 @@ class MyRobot(wpilib.IterativeRobot):
         # joystick 1 on the driver station
         self.stick = wpilib.Joystick(1)
 
-        # Initialization of the pneumatic system
-        self.Compressor = wpilib.Compressor(0)
-        self.Compressor.setClosedLoopControl(True)
-        self.enabled = self.Compressor.enabled()
-        self.PSV = self.Compressor.getPressureSwitchValue()
-        self.DS = wpilib.DoubleSolenoid(0, 1)
-        self.Compressor.start()
-
         # Initialization of the camera server
         wpilib.CameraServer.launch()
 
+        # Initialization of the ultrasonic sensor
+        self.AnalogInput = wpilib.AnalogInput(2)
+
+
     def autonomousInit(self):
-        """This function is run once each time the robot enters autonomous mode."""
-        self.timer.stop()
+        '''This function is run once each time the robot enters autonomous mode.'''
+        self.timer.reset()
         self.timer.start()
 
+
     def autonomousPeriodic(self):
-        """This function is called periodically during autonomous."""
-        if self.timer < 4.0:
-            self.drive.tankDrive(0.5, 0.5)
+        '''This function is called periodically during autonomous.'''
+
+        # Gets distance in the form of voltage from ultrasonic sensor.
+        distance = self.AnalogInput.getVoltage()
+
+        # Uses voltage to meter to determine the distance to obstacle
+        if distance > 0.75:
+            self.drive.tankDrive(0.55, 0.5)
         else:
             self.drive.tankDrive(0, 0)
+
+        gameData = DriverStation.getGameSpecificMessage()
+        position = DriverStation.getLocation()
+
+        def l():
+            if self.timer.get() < 5.0:
+                self.drive.tankDrive(0.5, 0.5)  # Drive forwards for 5 seconds at half speed.
+            elif 5.0 < self.timer.get() < 6.0:
+                self.drive.tankDrive(0, 0.5)  # Turn right for 1 second at half speed.
+            elif self.timer.get() > 6.0:
+                self.drive.tankDrive(0, 0)  # Stop after 6 seconds.
+            else:
+                self.drive.tankDrive(0, 0)
+
+        def r():
+            if self.timer.get() < 5.0:
+                self.drive.tankDrive(0.5, 0.5)  # Drive forwards for 5 seconds at half speed.
+            elif 5.0 < self.timer.get() < 6.0:
+                self.drive.tankDrive(0.5, 0)  # Turn right for 1 second at half speed.
+            elif self.timer.get() > 6.0:
+                self.drive.tankDrive(0, 0)  # Stop after 6 seconds.
+            else:
+                self.drive.tankDrive(0, 0)
+
+        def s():
+            if self.timer.get() < 5.0:
+                self.drive.tankDrive(0.5, 0.5)  # Drive forwards for 5 seconds at half speed.
+            elif self.timer.get() > 5.0:
+                self.drive.tankDrive(0, 0)  # Stop after 6 seconds.
+            else:
+                self.drive.tankDrive(0, 0)
+
+        def cs():
+            if self.timer.get() < 4.0:
+                self.drive.tankDrive(0.5, 0.5)  # Drive forwards for 5 seconds at half speed.
+            elif self.timer.get() > 4.0:
+                self.drive.tankDrive(0, 0)  # Stop after 6 seconds.
+            else:
+                self.drive.tankDrive(0, 0)
+
+        if gameData == "LLR" and position == 1:
+            l()
+        elif gameData == "LLR" and position == 2:
+            cs()
+        elif gameData == "LLR" and position == 3:
+            s()
+        elif gameData == "LRL" and position == 1:
+            l()
+        elif gameData == "LRL" and position == 2:
+            cs()
+        elif gameData == "LRL" and position == 3:
+            s()
+        elif gameData == "RLL" and position == 1:
+            s()
+        elif gameData == "RLL" and position == 2:
+            cs()
+        elif gameData == "RLL" and position == 3:
+            r()
+        elif gameData == "RLR" and position == 1:
+            s()
+        elif gameData == "RLR" and position == 2:
+            cs()
+        elif gameData == "RLR" and position == 3:
+            r()
+        else:
+            cs()
+
 
     def teleopInit(self):
         '''Executed at the start of teleop mode'''
@@ -59,7 +127,7 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         '''Runs the motors with tank steering'''
-        left_stick = self.stick.getRawAxis(1)/1.5
+        left_stick = self.stick.getRawAxis(1)/1.5   # Slows down motors
         right_stick = self.stick.getRawAxis(5)/1.5
         '''if right_stick < 0:
             divisor = 2
@@ -70,15 +138,6 @@ class MyRobot(wpilib.IterativeRobot):
 
         self.drive.tankDrive(-left_stick, -right_stick)
 
-        # Basic pneumatic operations using joystick
-        if self.stick.getRawButton(5):
-            self.DS.set(DoubleSolenoid.Value.kForward)
-        elif self.stick.getRawButton(6):
-            self.DS.set(DoubleSolenoid.Value.kReverse)
-        elif self.stick.getRawButton(1):
-            self.Compressor.stop()
-        elif self.stick.getRawButton(4):
-            self.Compressor.start()
 
 
 if __name__ == '__main__':
