5161c19cf420dc28233539c3de42f4e4b8b66319
Parsia B <parsiabahrami80@gmail.com>
2018-02-13 16:46:59 -0500

    Rev1.2.6
    Added proper intake and lift control objects. Fixed obtaining position in autonomous.

--- robot.py
+++ robot.py
@@ -20,23 +20,19 @@ class MyRobot(wpilib.IterativeRobot):
         self.rearLeftMotor = wpilib.Victor(3)
 
         # object that handles basic intake operations
-        self.omnomLeft = wpilib.Spark(7)
+        self.omnomLeft = wpilib.Spark(7)    # make sure channels are correct
         self.omnomRight = wpilib.Spark(8)
+
+        # object that handles basic climb operations
+        self.liftMotor = wpilib.Spark(5)    # make sure channel is correct
 
         # defining motor groups
         self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
         self.right = wpilib.SpeedControllerGroup(self.frontRightMotor, self.rearRightMotor)
 
-        # defining intake/output motor groups
-        self.omnom = wpilib.SpeedControllerGroup(self.omnomLeft, self.omnomRight)
-
         # setting up drive group for drive motors
         self.drive = DifferentialDrive(self.left, self.right)
         self.drive.setExpiration(0.1)
-
-        # setting up drive group for intake motors
-        self.omnomop = DifferentialDrive(self.omnom)
-        self.omnom.setExpiration(0.1)
 
         # defines timer for autonomous
         self.timer = wpilib.Timer()
@@ -44,10 +40,10 @@ class MyRobot(wpilib.IterativeRobot):
         # joystick 1 on the driver station
         self.stick = wpilib.Joystick(1)
 
-        # Initialization of the camera server
+        # initialization of the camera server
         wpilib.CameraServer.launch()
 
-        # Initialization of the ultrasonic sensor
+        # initialization of the ultrasonic sensor
         self.AnalogInput = wpilib.AnalogInput(2)
 
 
@@ -69,8 +65,10 @@ class MyRobot(wpilib.IterativeRobot):
         else:
             self.drive.tankDrive(0, 0)
 
-        gameData = DriverStation.getInstance().getGameSpecificMessage() #Gets color of switches and scale in string form
-        position = 3    # Change position based on need
+        # Gets color of switches and scale in string form
+        gameData = DriverStation.getInstance().getGameSpecificMessage()
+        # Gets position of robot on the field
+        position = DriverStation.getInstance().getLocation()
 
         def l():
             if self.timer.get() < 3.0:
@@ -101,7 +99,7 @@ class MyRobot(wpilib.IterativeRobot):
                 self.drive.tankDrive(0, 0)
 
         def cs():
-            if self.timer.get() < 4.0:
+            if self.timer.get() < +.0:
                 self.drive.tankDrive(0.5, 0.5)  # Drive forwards for 5 seconds at half speed.
             elif self.timer.get() > 4.0:
                 self.drive.tankDrive(0, 0)  # Stop after 6 seconds.
@@ -142,11 +140,30 @@ class MyRobot(wpilib.IterativeRobot):
 
     def teleopPeriodic(self):
         '''Runs the motors with tank steering'''
+
+        # controller mapping for tank steering
         left_stick = self.stick.getRawAxis(1)/2   # Slows down motors
         right_stick = self.stick.getRawAxis(5)/2
 
-        left_trigger = self.stick.getRawButton(2)
-        right_trigger = self.stick.getRawButton(3)
+        # basic intake controller mapping with relative speed
+        if self.stick.getRawButton(2):
+            self.omnomLeft.set(-0.5)
+            self.omnomRight.set(-0.5)
+        elif self.stick.getRawButton(3):
+            self.omnomLeft.set(0.5)
+            self.omnomRight.set(0.5)
+        else:
+            self.omnomLeft.set(0)
+            self.omnomRight.set(0)
+
+        # basic lift controller mapping with relative speed
+        if self.stick.getRawButton(5):
+            self.liftMotor.set(0.5)
+        elif self.stick.getRawButton(6):
+            self.liftMotor.set(-0.5)
+        else:
+            self.liftMotor.set(0)
+
         '''
         if right_stick < 0:
             divisor = 2
@@ -156,7 +173,7 @@ class MyRobot(wpilib.IterativeRobot):
         adjusted_right_stick = right_stick/divisor
         '''
         self.drive.tankDrive(-left_stick, -right_stick)
-        self.omnomop.arcadeDrive(-left_trigger, right_trigger)
+
 
 
 if __name__ == '__main__':
